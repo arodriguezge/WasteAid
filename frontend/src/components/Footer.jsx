ccc28d84325e29b65bd2c7722362f389c49956d2
Gabriela Oliveira <gabriela.oliveira.bpt@gmail.com>
2019-02-04 13:32:43 +0100

    updating_components

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    // constructor(props) {
+    //     super(props)
+    //     this.state = {
+    //     }
+    // }
+
+
+    render() {
+        return (
+            <React.Fragment>
+                <footer className="page-footer font-small bg-success pt-4">
+                    <div className="footer-copyright text-center py-3 text-white">
+                        © 2019 Copyright: Recycle Database
+                    </div>
+                </footer>
+
+            </React.Fragment>
+        )
+    }
+}
+
+export default Footer;
