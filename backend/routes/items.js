eac8dfe58e39c88b1dcf12356a3a9530bf00299f
Andrea/Rodriguez/Germann <andrearodriguezgermann@gmaiil.com>
2019-02-05 10:55:42 +0100

    created items controller and routes

--- backend/routes/items.js
+++ backend/routes/items.js
@@ -1,15 +1,23 @@
 const express = require('express')
 const router = express.Router()
 
-// const {} = require('../controllers/ItemsController')  
+const {
+    index,
+    newItem,
+    getItem,
+    editItem,
+    removeItem
+} = require('../controllers/itemsController')  
 
 // GET all items
-router.get('/', (req, res) => {
-    res.send('all items')
-})
+router.get('/', index)
+// POST create item create
+router.post('/create', newItem)
 // GET specific item
-router.get('/:id', (req, res) => {
-    res.send('single item')
-})
+router.get('/:id', getItem)
+// PATCH edit item
+router.patch('/edit/:id', editItem)
+// DELETE item
+router.delete('/remove/:id', removeItem)
 
 module.exports = router
