5cb079490815a7feb6343df2783c49b1a5938b34
Androklus <philipp-langer@gmx.de>
2019-02-12 13:11:39 +0100

    changed item.jsx, changed style.scss, added a folder and 7 images to public folder, added itemCard.scss

--- frontend/src/components/Item.jsx
+++ frontend/src/components/Item.jsx
@@ -1,16 +1,104 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
-class Item extends Component {
+// corresponding style file: _itemCard.scss
+
+class ItemDescription extends Component {
     render() {
         return (
-            <div className="container">
-                <h3>Item:</h3>
-                <h5>{this.props.item.name}</h5>
-                <p>{this.props.item.description}</p>
-                <h6>{this.props.item.bin}</h6>
+            <div>
+                {this.props.descr}
             </div>
-        );
+        )
     }
 }
 
-export default Item
+class ItemCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            itemDescrVisible: false,
+            toggleSwitchChar: String.fromCharCode(9660),
+            toggleHint: "show item description"
+        };
+        this.toggleItemDescr = this.toggleItemDescr.bind(this);
+    }
+
+    toggleItemDescr() {
+        if (this.state.itemDescrVisible === false) {
+            this.setState({
+                itemDescrVisible: true,
+                toggleSwitchChar: String.fromCharCode(9650),
+                toggleHint: "hide item description"
+            })
+        } else {
+            this.setState({
+                itemDescrVisible: false,
+                toggleSwitchChar: String.fromCharCode(9660),
+                toggleHint: "show item description"
+            })
+        }
+    }
+
+    binIconPath() {
+        //console.log(this.props.item.description);
+        switch(this.props.item.bin) {
+            case 'blue':
+                return "../images/bin-blue-no-label.svg";
+            case 'green':
+                return "../images/bin-green-no-label.svg";
+            case 'white':
+                return "../images/bin-white-no-label.svg";
+            case 'yellow':
+                return "../images/bin-yellow-no-label.svg";
+            case 'brown':
+                return "../images/bin-brown-no-label.svg";
+            case 'grey':
+                return "../images/bin-darkgray-no-label.svg";
+            case 'none':
+                return "../images/bin-red-no-label-no-hole.svg";
+            default:
+                return null;
+        }
+    };
+
+    binIconTitle() {
+        switch(this.props.item.bin) {
+            case 'blue':
+                return "bin for paper and cardboard";
+            case 'green':
+                return "bin for green glass";
+            case 'white':
+                return "bin for white glass";
+            case 'yellow':
+                return "bin for plastic, metal, Green Dot";
+            case 'brown':
+                return "bin for bio waste";
+            case 'grey':
+                return "bin for residual waste";
+            case 'none':
+                return "none of the bins, separate disposal";
+            default:
+                return null;
+        }
+    };
+
+    render() {
+        return (
+            <div className="item-card">
+                <div>
+                    <div className="card-title">{this.props.item.name}</div>
+                    <div className="desrc-toggle-switch" onClick={this.toggleItemDescr} title={this.state.toggleHint}>
+                        &nbsp;{this.state.toggleSwitchChar}
+                    </div>
+                    { this.state.itemDescrVisible ? <ItemDescription descr={this.props.item.description}/> : null }
+
+                </div>
+                <div>
+                    <img className="wastebin-icon" src={this.binIconPath()} alt="wastebin icon" title={this.binIconTitle()}/>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ItemCard
