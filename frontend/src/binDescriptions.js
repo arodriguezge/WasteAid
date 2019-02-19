a7b25fec66543008c72563b83f36848e9f793ae9
Androklus <philipp-langer@gmx.de>
2019-02-19 12:21:03 +0100

    added SearchArea, AddItemForm and corresponding scss file and images, updated navigation in Header, updated a path in Router

--- frontend/src/binDescriptions.js
+++ frontend/src/binDescriptions.js
@@ -1,9 +1,9 @@
 export const binDescriptions = {
-    blue: "description blue waste bin, description blue waste bin, description blue waste bin, description blue waste bin",
-    green: "description green waste bin, description green waste bin, description green waste bin, description green waste bin",
-    white: "description white waste bin, description white waste bin, description white waste bin, description white waste bin",
-    yellow: "description yellow waste bin, description yellow waste bin, description yellow waste bin, description yellow waste bin",
-    brown: "description brown waste bin, description brown waste bin, description brown waste bin, description brown waste bin",
-    grey: "description grey waste bin, description grey waste bin, description grey waste bin, description grey waste bin",
-    none: "description separate waste disposal, description separate waste disposal, description separate waste disposal"
+    blue: "paper, e.g. newspapers, paper packaging, books, booklets, prospectus, cardboards",
+    green: "green glass, e.g. perfume bottles, jars, bottles for juice, wine, vinegar, oil",
+    white: "white glass, e.g. perfume bottles, jars, bottles for juice, wine, vinegar, oil",
+    yellow: "reusable materials, e.g. plastic bottles, foil, tin cans, tooth brushes, yogurt pots",
+    brown: "bio, e.g. flowers, garden rubbish, food waste, kitchen paper, rancid food without wrap",
+    grey: "household rubbish, e.g. ash, wallpapers, dust bags, dish ware, animal litter, window glass",
+    none: "nuclear waste, e.g. weapons-grade plutonium designated for permanent repository"
 };
