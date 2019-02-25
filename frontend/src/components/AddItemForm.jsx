f5ba6bca7dc8a8776741b94cc4c87a1501decd19
Gabriela Oliveira <gabriela.oliveira.bpt@gmail.com>
2019-02-25 18:05:20 +0100

    'adding changes'

--- frontend/src/components/AddItemForm.jsx
+++ frontend/src/components/AddItemForm.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Header from './Header'
 import Footer from './Footer'
 
@@ -6,9 +6,9 @@ class FormSubmitHint extends Component {
     render() {
         return (
             <div className="form-submit-hint">
-                <p className="close-hint-char" onClick={this.props.hideHint} title="Close pop-up">&#10005;</p><br/>
-                Thank you!<br/>
-                Your proposal has been submitted.<br/>
+                <p className="close-hint-char" onClick={this.props.hideHint} title="Close pop-up">&#10005;</p><br />
+                Thank you!<br />
+                Your proposal has been submitted.<br />
                 Approval pending.
             </div>
         )
@@ -47,38 +47,40 @@ class AddItemForm extends Component {
     render() {
         return (
             <React.Fragment>
-                <Header/>
-                <div className="container">
-                    <h4>Send us a new item</h4>
+                <Header />
+                <div className="footer-fix">
+                    <div className="container">
+                        <h4>Send us a new item</h4>
 
-                    <form onSubmit={this.handleSubmit} id="add-item-form">
-                        <p>Waste Item Name:</p>
-                        <input type="text" className="form-control" placeholder="Type name of waste item here..."
-                               required={true} ref={this.name}/><br/> {/* required={true} */}
+                        <form onSubmit={this.handleSubmit} id="add-item-form">
+                            <p>Waste Item Name:</p>
+                            <input type="text" className="form-control" placeholder="Type name of waste item here..."
+                                required={true} ref={this.name} /><br /> {/* required={true} */}
 
-                        <p className="heading">Waste Item Description:</p><br/>
-                        <textarea type="text" className="form-control" id="exampleFormControlTextarea1" rows="5"
-                                  placeholder="Type item description here..." ref={this.description}/><br/>
+                            <p className="heading">Waste Item Description:</p><br />
+                            <textarea type="text" className="form-control" id="exampleFormControlTextarea1" rows="5"
+                                placeholder="Type item description here..." ref={this.description} /><br />
 
-                        <p className="heading">Waste Bin:</p><br/>
-                        <select defaultValue="Choose..." ref={this.bin}>
-                            <option value="Choose...">Choose...</option>
-                            <option value="blue">blue bin (paper, cardboard)</option>
-                            <option value="green">green bin (colored glass)</option>
-                            <option value="white">white bin (white glass)</option>
-                            <option value="yellow">yellow/orange bin (plastic, metal, Green Dot)</option>
-                            <option value="brown">brown bin (biodegradable goods)</option>
-                            <option value="grey">grey/black bin (everything else)</option>
-                            <option value="none">none of the bins (other waste disposal)</option>
-                        </select><br/>
+                            <p className="heading">Waste Bin:</p><br />
+                            <select defaultValue="Choose..." ref={this.bin}>
+                                <option value="Choose...">Choose...</option>
+                                <option value="blue">blue bin (paper, cardboard)</option>
+                                <option value="green">green bin (colored glass)</option>
+                                <option value="white">white bin (white glass)</option>
+                                <option value="yellow">yellow/orange bin (plastic, metal, Green Dot)</option>
+                                <option value="brown">brown bin (biodegradable goods)</option>
+                                <option value="grey">grey/black bin (everything else)</option>
+                                <option value="none">none of the bins (other waste disposal)</option>
+                            </select><br />
 
-                        <button className="btn btn-secondary" type="submit">Add item</button>
-                        <button className="btn btn-secondary" onClick={this.clearForm}>Reset form</button>
-                    </form>
+                            <button className="btn btn-secondary" type="submit">Add item</button>
+                            <button className="btn btn-secondary" onClick={this.clearForm}>Reset form</button>
+                        </form>
 
-                    {!this.state.isHidden && <FormSubmitHint hideHint={this.toggleHidden}/>}
+                        {!this.state.isHidden && <FormSubmitHint hideHint={this.toggleHidden} />}
+                    </div>
                 </div>
-                <Footer/>
+                <Footer />
             </React.Fragment>
         )
     }
