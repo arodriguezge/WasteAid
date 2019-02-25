eede7cc66bb4b154c14b544d030450e48ebad3de
Androklus <philipp-langer@gmx.de>
2019-02-25 15:26:21 +0100

    styling item list, corresponding changes in css/style.css happened automatically

--- frontend/src/components/ItemsList.jsx
+++ frontend/src/components/ItemsList.jsx
@@ -1,19 +1,27 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import Item from './Item'
+import Footer from './Footer'
+import Header from './Header'
+
+// corresponding style file: _ticketList.scss
 
 class ItemList extends Component {
 
     render() {
         return (
-            <div>
-                <h1>Approved Items</h1>
-                {Object.keys(this.props.items).map((index) =>{
-                    if(this.props.items[index].approved === true) {
-                        return <Item item={this.props.items[index]} key={`item ${index}`} />
-                    }
-                    return ''
-                })}
-            </div>
+            <React.Fragment>
+                <Header/>
+                <div className="container">
+                    <h4  className="h4-5">Approved Items</h4>
+                    {Object.keys(this.props.items).map((index) => {
+                        if (this.props.items[index].approved === true) {
+                            return <Item item={this.props.items[index]} key={`item ${index}`}/>
+                        }
+                        return ''
+                    })}
+                </div>
+                <Footer/>
+            </React.Fragment>
         )
     }
 }
