60fb1223f23aab1711b4f4d2d1325673f7bf6c81
Gabriela Oliveira <gabriela.oliveira.bpt@gmail.com>
2019-02-28 17:35:11 +0100

    'cleaning the code'

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -41,43 +41,9 @@ class Header extends React.Component {
                             </li>
                         </ul>
 
-                        {/* <a className="nav-item active nav-link" href="#">Home
-                                <span className="sr-only">(current)</span>
-                            </a>
-                            <a className="nav-item nav-link" href="#">
-
-                            </a>
-                            <a className="nav-item nav-link" href="#">Maison</a>
-                            <a className="nav-item nav-link" href="#">Haus</a> */}
 
                     </div>
-                    {/* <div className="container-fluid md-tabs "> */}
-                    {/* <div className="d-flex img-fluid justify-content-start align-items-center">
-                            <span className="display-4 text-dark">RECYCLE </span> &nbsp;
-                            <img src={component} className=" mx-auto " width="50" height="50" alt="waste bin icon" />
-                            &nbsp;
-                            <span className="display-4 text-dark"> DATABASE</span>
-                        </div> */}
 
-                    {/* <ul className="nav  text-white justify-content-end lighten-1 mx-0 mb-0 mt-1 pt-4 pb-2">
-                            <li className="nav-item">
-                                <NavLink to="/" className="nav-link nav-text text-white">Home</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink to="/searchArea" className="nav-link nav-text text-white">Search Area</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink to="/add" className="nav-link nav-text text-white">Add an Item</NavLink>
-
-                            </li>
-                            <li className="nav-item">
-                                <NavLink to="/recommendedLinks" className="nav-link nav-text text-white">Recommended Links</NavLink>
-                            </li>
-                            <li className="nav-item">
-                                <NavLink to="/aboutUs" className="nav-link nav-text text-white">About us</NavLink>
-                            </li>
-                        </ul>
-                    </div> */}
                 </nav>
             </React.Fragment>
         )
