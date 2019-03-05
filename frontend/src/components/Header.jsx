4bbb9e686dd1be6e2a1bceceee0a4570ee40007e
Androklus <philipp-langer@gmx.de>
2019-03-05 12:51:07 +0100

    Fixed two footer problems (jumping, z-order). I didn't modify the header.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,25 +1,41 @@
 import React from 'react'
 import component from '../images/bin-red-no-label-no-lines.svg'
+import component2 from '../images/bin-yellow-no-label.svg'
+import menuButton from '../images/menu-button.svg'
 import { NavLink } from 'react-router-dom'
 
 
 class Header extends React.Component {
+    state = {
+        navCollapsed: true
+    }
+
+    onToggleNav = () => {
+        this.setState({ navCollapsed: !this.state.navCollapsed })
+    }
 
     render() {
+        const { navCollapsed } = this.state
+
         return (
             <React.Fragment>
-                <nav className="navbar navbar-inverse bg-success">
-                    <div className="container-fluid md-tabs ">
-                        <div className="d-flex img-fluid justify-content-start align-items-center">
-                            <span className="display-4 text-dark">RECYCLE </span> &nbsp;
-                            <img src={component} className=" mx-auto " width="50" height="50" alt="waste bin icon" />
-                            &nbsp;
-                            <span className="display-4 text-dark"> DATABASE</span>
-                        </div>
+                <nav className="navbar navbar-expand-md bg-success">
+                    <span className="navbar-brand">
+                        <img src={component} className="mx-auto" width="30" height="30" alt="something" />
+                        <img src={component2} className="mx-auto" width="40" height="30" alt="something" />
+                        <img src={component} className=" mx-auto " width="30" height="30" alt="something" />
+                    </span>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarLinks" aria-controls="navbarLinks"
+                            aria-expanded="false" aria-label="Toggle navigation" onClick={this.onToggleNav}>
+                        <img src={menuButton} className="mx-auto" width="30" height="30" alt="something" />
+                        {/* <span className="navbar-toggler-icon"></span> */}
+                    </button>
+                    <div id="navbarLinks" className={(navCollapsed ? 'collapse justify-content-end' : '') + ' navbar-collapse '}
+                    >
 
-                        <ul className="nav  text-white justify-content-end lighten-1 mx-0 mb-0 mt-1 pt-4 pb-2">
+                        <ul className="navbar-nav">
                             <li className="nav-item">
-                                <NavLink to="/" className="nav-link nav-text text-white">Home</NavLink>
+                                <NavLink to="/" className="nav-link nav-text text-white active">Home</NavLink>
                             </li>
                             <li className="nav-item">
                                 <NavLink to="/searchArea" className="nav-link nav-text text-white">Search Area</NavLink>
@@ -35,7 +51,10 @@ class Header extends React.Component {
                                 <NavLink to="/aboutUs" className="nav-link nav-text text-white">About us</NavLink>
                             </li>
                         </ul>
+
+
                     </div>
+
                 </nav>
             </React.Fragment>
         )
