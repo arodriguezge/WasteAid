d9921b83f53e48e73fd68a032f3791b078a8e926
Gabriela Oliveira <gabriela.oliveira.bpt@gmail.com>
2019-03-07 19:11:46 +0100

    transforming some class components into function components and updating app name

--- frontend/src/components/AboutUs.jsx
+++ frontend/src/components/AboutUs.jsx
@@ -2,34 +2,33 @@ import React from 'react'
 import Footer from './Footer'
 import Header from './Header'
 
-class AboutUs extends React.Component {
-    render() {
-        return (
-            <React.Fragment>
-                <Header/>
-                    <div className="container">
+const AboutUs = props => {
 
-                        <h1 className="display-5 ml-5 pt-4 mb-5 mt-4">Who we are</h1>
-                        <div className="container">
-                            <p>We care about managing your waste and how you are managing your waste, the easy
-                                way. <br/>
-                                Getting rid of your old wallpaper? Look up where it goes. Our app “Recycling
-                                Database” <br/>
-                                comes in handy for just that.
-                                <br/><br/>
-                                Technologies we used, got to work with, and learned about with in the process:
-                                <br/><br/>
-                                Frontend: React, Bootstrap, axios<br/>
-                                Backend: Express, MongoDB, Mongoose
-                                <br/>
-                            </p>
-                        </div>
-                      
+    return (
+        <React.Fragment>
+            <Header />
+            <div className="container">
+
+                <h1 className="display-5 ml-5 pt-4 mb-5 mt-4">Who we are</h1>
+                <div className="container">
+                    <div>We care about managing your waste and how you are managing your waste, the easy
+                                way. <br />
+                        Getting rid of your old wallpaper? Look up where it goes. Our app “Waste Disposal Advisor” <br />
+                        comes in handy for just that.
+                                <br /><br />
+                        Technologies we used, got to work with, and learned about with in the process:
+                                <br /><br />
+                        Frontend: Javascript, React, Redux, Bootstrap, axios<br />
+                        Backend: Node.js, Express, MongoDB, Mongoose
+                                <br />
                     </div>
-                <Footer/>
-            </React.Fragment>
-        )
-    }
+                </div>
+
+            </div>
+            <Footer />
+        </React.Fragment>
+    )
 }
+
 
 export default AboutUs
