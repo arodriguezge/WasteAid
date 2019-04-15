4f6c0c760fbbaeb41b56998ffda6da7b32e4b9d9
Gabriela Oliveira <gabriela.oliveira.bpt@gmail.com>
2019-04-15 19:22:43 +0200

    'editing header'

--- frontend/src/components/Header2.jsx
+++ frontend/src/components/Header2.jsx
@@ -13,17 +13,19 @@ class Header extends React.Component {
 
             <React.Fragment>
                 <Navbar bg="success" expand="lg">
+
                     <Navbar.Brand className="pb-0 pt-0">
                         <img src={logo} className="mx-auto" width="50" height="50" alt="logo" />&nbsp; &nbsp;
                         <span className="text-white font-weight-bold d-sm-inline d-none">Waste Disposal Advisor</span>
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-                        <Nav className="mr-auto">
-                            <Nav.Link className="nav-text text-grey" href="/" exact activeClassName="active"><span className="pb-1">Home</span></Nav.Link>
-                            <Nav.Link className="nav-text text-grey" href="/searchArea"><span className="pb-1">Search Area</span></Nav.Link>
-                            <Nav.Link className="nav-text text-grey" href="/mapSearch"><span className="pb-1">Map Search</span></Nav.Link>
-                            <Nav.Link className="nav-text text-grey" href="/add">Add an Item</Nav.Link>
+
+                        <Nav className="">
+                            <Nav.Link className="nav-text text-grey" href="/" exact activeClassName="active"><span className="pb-1 hover">Home</span></Nav.Link>
+                            <Nav.Link className="nav-text text-grey" href="/searchArea"><span className="pb-1 hover">Search Area</span></Nav.Link>
+                            <Nav.Link className="nav-text text-grey" href="/mapSearch"><span className="pb-1 hover">Map Search</span></Nav.Link>
+                            <Nav.Link className="nav-text text-grey" href="/add"><span className="pb-1 hover">Add an Item</span></Nav.Link>
 
 
                             <NavDropdown alignRight title="Learn More" className="nav-text text-grey" id="basic-nav-dropdown">
