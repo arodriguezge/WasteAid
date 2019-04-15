f05620c07d18cfc3b40d8a5ce112ae61d61a8371
Gabriela Oliveira <gabriela.oliveira.bpt@gmail.com>
2019-04-15 15:18:02 +0200

    'editing header'

--- frontend/src/components/Header2.jsx
+++ frontend/src/components/Header2.jsx
@@ -1,80 +1,46 @@
 import React from 'react'
 import logo from '../images/WDA-Logo-grey-orange-green-blue.svg'
-import menuButton from '../images/menu-button.svg'
-import { NavLink } from 'react-router-dom'
-import { Navbar, Button, NavDropdown, Form, FormControl, Nav } from 'react-bootstrap'
+//import menuButton from '../images/menu-button.svg'
+//import { NavLink } from 'react-router-dom'
+import { Navbar, NavDropdown, Nav } from 'react-bootstrap'
 
 
 
 class Header extends React.Component {
-    state = {
-        navCollapsed: true
-    }
-
-
-    onToggleNav = () => {
-        this.setState({ navCollapsed: !this.state.navCollapsed })
-    }
-
-
     render() {
-
-        const { navCollapsed } = this.state
 
         return (
 
             <React.Fragment>
-                <Navbar bg="light" expand="lg">
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar bg="success" expand="lg">
+                    <Navbar.Brand className="pb-0 pt-0">
+                        <img src={logo} className="mx-auto" width="50" height="50" alt="logo" />&nbsp; &nbsp;
+                        <span className="text-white font-weight-bold d-sm-inline d-none">Waste Disposal Advisor</span>
+                    </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
+                    <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
                         <Nav className="mr-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            <Nav.Link className="nav-text text-grey" href="/" exact activeClassName="active"><span className="pb-1">Home</span></Nav.Link>
+                            <Nav.Link className="nav-text text-grey" href="/searchArea"><span className="pb-1">Search Area</span></Nav.Link>
+                            <Nav.Link className="nav-text text-grey" href="/mapSearch"><span className="pb-1">Map Search</span></Nav.Link>
+                            <Nav.Link className="nav-text text-grey" href="/add">Add an Item</Nav.Link>
+
+
+                            <NavDropdown alignRight title="Learn More" className="nav-text text-grey" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="/learnMore">FAQ</NavDropdown.Item>
+                                <NavDropdown.Item href="/aboutUs">About Us</NavDropdown.Item>
+
+
                             </NavDropdown>
+
+
                         </Nav>
-                        <Form inline>
-                            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                            <Button variant="outline-success">Search</Button>
-                        </Form>
+
                     </Navbar.Collapse>
-                </Navbar>;
+                </Navbar>
             </React.Fragment>
         )
     }
 }
 
 export default Header
-{/* <Navbar bg="light" expand="lg">
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#link">Link</Nav.Link>
-      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
-    </Nav>
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-success">Search</Button>
-    </Form>
-  </Navbar.Collapse>
-</Navbar>;
-
-import Navbar from 'react-bootstrap/Navbar';
-
-// or less ideally
-import { Button } from 'react-bootstrap';
-*/}
