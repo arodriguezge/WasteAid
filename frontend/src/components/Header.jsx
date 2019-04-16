55198946f94722ee0777a0b6db516180ee6a8fca
Gabriela Oliveira <gabriela.oliveira.bpt@gmail.com>
2019-04-16 12:15:34 +0200

    'editing header'

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import logo from '../images/WDA-Logo-grey-orange-green-blue.svg'
-import { Navbar, NavDropdown, Nav, Dropdown, NavItem, NavLink } from 'react-bootstrap'
+import { Navbar, NavDropdown, Nav } from 'react-bootstrap'
 
 class Header extends React.Component {
     render() {
@@ -18,10 +18,10 @@ class Header extends React.Component {
                     <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
 
                         <Nav className="">
-                            <Nav.Link className="nav-text text-grey" href="/" exact activeClassName="active"><span className="pb-1 hover">Home</span></Nav.Link>
-                            <Nav.Link className="nav-text text-grey" href="/searchArea"><span className="pb-1 hover">Search Area</span></Nav.Link>
-                            <Nav.Link className="nav-text text-grey" href="/mapSearch"><span className="pb-1 hover">Map Search</span></Nav.Link>
-                            <Nav.Link className="nav-text text-grey" href="/add"><span className="pb-1 hover">Add an Item</span></Nav.Link>
+                            <Nav.Link className="nav-text text-grey" href="/" exact="true" activeclassname="active"><span className="pb-1 hover">Home</span></Nav.Link>
+                            <Nav.Link className="nav-text text-grey" href="/searchArea" activeclassname="active"><span className="pb-1 hover">Search Area</span></Nav.Link>
+                            <Nav.Link className="nav-text text-grey" href="/mapSearch" activeclassname="active"><span className="pb-1 hover">Map Search</span></Nav.Link>
+                            <Nav.Link className="nav-text text-grey" href="/add" activeclassname="active"><span className="pb-1 hover">Add an Item</span></Nav.Link>
                             <NavDropdown alignRight title="Learn More" className="nav-text text-grey" id="basic-nav-dropdown">
                                 <NavDropdown.Item href="/learnMore">FAQ</NavDropdown.Item>
                                 <NavDropdown.Item href="/aboutUs">About Us</NavDropdown.Item>
