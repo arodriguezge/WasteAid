8e5adf28e679a17f6c579a838b7796706e0e7d1c
Gabriela Oliveira <gabriela.oliveira.bpt@gmail.com>
2019-04-17 10:46:57 +0200

    'added functionality to  navbar'

--- frontend/src/components/AdminFrame.jsx
+++ frontend/src/components/AdminFrame.jsx
@@ -13,6 +13,7 @@ class AdminFrame extends Component {
                 <HeaderAdmin />
                 <div className="container">
                     <h2 className="text-center pt-4 font-weight-bold">Admin Frame</h2>
+                    <p className="text-center">Click in the links above to display Admin content</p>
                 </div>
                 <Footer />
             </React.Fragment>
--- frontend/src/components/HeaderAdmin.jsx
+++ frontend/src/components/HeaderAdmin.jsx
@@ -17,9 +17,10 @@ class HeaderAdmin extends React.Component {
                     <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
 
                         <Nav className="">
-                            <Nav.Link className="nav-text text-grey" href="/" exact="true" activeclassname="active"><span className="pb-1 hover">Admin Tickets</span></Nav.Link>
-                            <Nav.Link className="nav-text text-grey" href="/searchArea/default" activeclassname="active"><span className="pb-1 hover">Tickets Aproved</span></Nav.Link>
-                            <Nav.Link className="nav-text text-grey" href="/mapSearch/default" activeclassname="active"><span className="pb-1 hover">Map Tickets</span></Nav.Link>
+                            <Nav.Link className="nav-text text-grey" href="/admin/tickets" exact="true" activeclassname="active"><span className="pb-1 hover">Admin Tickets</span></Nav.Link>
+                            <Nav.Link className="nav-text text-grey" href="/items" activeclassname="active"><span className="pb-1 hover">Tickets Aproved</span></Nav.Link>
+                            {/* map is disabled, because I am not sure what to display */}
+                            <Nav.Link className="nav-text text-grey" eventKey="/mapSearch/default" activeclassname="active" ><span className="pb-1 hover">Map Tickets</span></Nav.Link>
 
                         </Nav>
 
